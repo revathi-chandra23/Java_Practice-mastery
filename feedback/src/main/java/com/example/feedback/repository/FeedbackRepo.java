56228adb1f12386a58cdb030eb8791f3c98d0a1a
revathi-chandra23 <revathichandra2302@gmail.com>
2026-03-23 09:49:59 +0530

    making comments in the  feedback

--- feedback/src/main/java/com/example/feedback/repository/FeedbackRepo.java
+++ feedback/src/main/java/com/example/feedback/repository/FeedbackRepo.java
@@ -7,7 +7,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
 public interface FeedbackRepo extends MongoRepository<FeedbackModel, String> {
-
+// Custom query to find feedback by product ID and rating with pagination
     @Query("{'productId': ?0, 'rating': ?1}")
     Page<FeedbackModel> findByProductIdAndRating(String productId, int rating, Pageable pageable);
 }
