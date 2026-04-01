9e45e442c1417423cb8bea67447e08a8b389daf9
revathi-chandra23 <revathichandra2302@gmail.com>
2026-04-01 10:50:36 +0530

    variables

---
+++ variables/variables/PrimitiveVariablesExample.java
@@ -0,0 +1,14 @@
+public class PrimitiveVariablesExample {
+    public static void main(String[] args) {
+
+        int a = 100;
+        double b = 10.5;
+        char c = 'A';
+        boolean isJavaFun = true;
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(isJavaFun);
+    }
+}
---
+++ variables/variables/ReferenceVariableExample.java
@@ -0,0 +1,7 @@
+public class ReferenceVariableExample {
+    public static void main(String[] args) {
+
+        String name = "Revathi"; // Reference variable
+        System.out.println(name);
+    }
+}
