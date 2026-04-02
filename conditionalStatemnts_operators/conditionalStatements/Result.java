9bfaa842466dfa0d7a878930b8557da743fda856
revathi-chandra23 <revathichandra2302@gmail.com>
2026-04-02 10:49:06 +0530

    if else statements

---
+++ conditionalStatemnts_operators/conditionalStatements/Result.java
@@ -0,0 +1,11 @@
+public class Result {
+    public static void main(String[] args) {
+        int marks = 45;
+
+        if (marks >= 35) {
+            System.out.println("Pass");
+        } else {
+            System.out.println("Fail");
+        }
+    }
+}
