a07d8f9cd68f73db9ae018d42e5c8ecdeb535f20
revathi-chandra23 <revathichandra2302@gmail.com>
2026-04-07 09:32:22 +0530

    oops part 2

--- jsonObjectFileREname/src/main/java/org/example/Main.java
+++ jsonObjectFileREname/src/main/java/org/example/Main.java
@@ -2,6 +2,7 @@ package org.example;
 
 class Main{
     public static final String FILE_PATH="jsonObjectFileREname\\src\\main\\resources\\data.json";
+    public static void main(String[] args) {
         ReName.doUpdate();
     }
 }
