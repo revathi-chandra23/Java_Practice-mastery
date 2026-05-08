9d7738900539489b063910c2b8cd4ce8c7832370
revathi-chandra23 <revathichandra2302@gmail.com>
2026-05-08 17:38:02 +0530

    streams

--- InterfacesJava/stream_Examples/NoneMatch.java
+++ InterfacesJava/stream_Examples/NoneMatch.java
@@ -4,6 +4,22 @@ public class NoneMatch {
         java.util.List<Integer> nums = java.util.Arrays.asList(1,2,3,4,5);
       System.out.println(  nums.stream()
                                .filter(n -> n > 3)
-                               .noneMatch(n->n%2==0));
+
+                               .noneMatch(n -> n % 2 == 0));
+                            
+
+
+        List<List<String>> names = Arrays.asList(
+            Arrays.asList("A", "B"),
+            Arrays.asList("C", "D")
+        );
+
+        List<String> result = names.stream()
+                                   .flatMap(List::stream)
+                                   .collect(Collectors.toList());
+
+        System.out.println(result);
     }
 }
+                            
+                            
